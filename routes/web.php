f4eb60be7e59c00d30bd24c3c302f858e5bd0e63
RomeoL <Gnaevs@gmail.com>
2021-02-14 14:04:59 -0600

    Cambios menores 2

--- routes/web.php
+++ routes/web.php
@@ -53,17 +53,19 @@
 
 
 
-Route::view('/', 'home')->name('home'); //es preferible hacerlo asi para paginas de poca informacion como: politicas de privacidad
-                            //O que contengan solo texto
+// Route::view('/', 'home')->name('home'); //es preferible hacerlo asi para paginas de poca informacion como: politicas de privacidad
+//                             //O que contengan solo texto
 
-//Tambien podemos agregar valores por ejemplo, poniendo un array en los parametros de la vista
+// //Tambien podemos agregar valores por ejemplo, poniendo un array en los parametros de la vista
 
-//Route::view('/','home', ['nombre' => 'Romeo']);
+// //Route::view('/','home', ['nombre' => 'Romeo']);
 
-Route::view('/about', 'about')->name('about');
-Route::view('/contact', 'contact')->name('contact');
-//Route::get('portfolio', 'PorfolioController'); //Aqui se esta llamando al controlador en lugar de la vista la vista se llama desde el controlador
-Route::get('/portfolio',[\App\Http\Controllers\PorfolioController::class, 'index'])->name('portfolio');
+// Route::view('/about', 'about')->name('about');
+// Route::view('/contact', 'contact')->name('contact');
+// //Route::get('portfolio', 'PorfolioController'); //Aqui se esta llamando al controlador en lugar de la vista la vista se llama desde el controlador
+// Route::get('/portfolio',[\App\Http\Controllers\PorfolioController::class, 'index'])->name('portfolio');
+
+Route::resource('proyectos', 'PorfolioController')->except(['index', 'show']);
 
 //---------------------------Blade--------------------------------------------------------------------
 //Blade es un motor de plantillas que trae incluido laravel
