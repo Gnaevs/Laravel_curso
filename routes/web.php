8299380083ea864ea7d9086d2bf0700e931056c6
RomeoL <Gnaevs@gmail.com>
2021-02-16 23:20:22 -0600

    Comenzando leccion para activar links de navegación

--- routes/web.php
+++ routes/web.php
@@ -53,19 +53,21 @@
 
 
 
-// Route::view('/', 'home')->name('home'); //es preferible hacerlo asi para paginas de poca informacion como: politicas de privacidad
-//                             //O que contengan solo texto
+Route::view('/', 'home')->name('home'); //es preferible hacerlo asi para paginas de poca informacion como: politicas de privacidad
+                            //O que contengan solo texto
 
-// //Tambien podemos agregar valores por ejemplo, poniendo un array en los parametros de la vista
+//Tambien podemos agregar valores por ejemplo, poniendo un array en los parametros de la vista
 
-// //Route::view('/','home', ['nombre' => 'Romeo']);
+//Route::view('/','home', ['nombre' => 'Romeo']);
 
-// Route::view('/about', 'about')->name('about');
-// Route::view('/contact', 'contact')->name('contact');
-// //Route::get('portfolio', 'PorfolioController'); //Aqui se esta llamando al controlador en lugar de la vista la vista se llama desde el controlador
-// Route::get('/portfolio',[\App\Http\Controllers\PorfolioController::class, 'index'])->name('portfolio');
+Route::view('/about', 'about')->name('about');
+Route::view('/contact', 'contact')->name('contact');
+//Route::get('portfolio', 'PorfolioController'); //Aqui se esta llamando al controlador en lugar de la vista la vista se llama desde el controlador
+Route::get('/portfolio','PortfolioController@index')->name('portfolio'); // Llamar el controlador resource
 
-Route::resource('proyectos', 'PorfolioController')->except(['index', 'show']);
+//Route::Resource('proyectos', 'PortfolioController');//Controlador Resource
+Route::apiResource('proyectos', 'PortfolioController');//Controlador api
+
 
 //---------------------------Blade--------------------------------------------------------------------
 //Blade es un motor de plantillas que trae incluido laravel
