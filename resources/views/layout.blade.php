84a8c06028eaafe50bb984b83b29ab80dcfcaef2
RomeoL <Gnaevs@gmail.com>
2021-02-17 18:57:59 -0600

    Cambio coloreado en el menu de navegacion

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,14 +2,21 @@
 <html>
 <head>
     <title>@yield('title', 'Romeo')</title>
+    <style>
+        .active a{
+            color: red;
+            text-decoration: none;
+        }
+    </style>
 </head>
 <body>
 <nav>
+    
         <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/portfolio">Portfolio</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li class="{{ request()->routeIs('home')? 'active' : ''}}"><a href="/">Home</a></li>
+            <li class="{{ request()->routeIs('about')? 'active' : ''}}"><a href="/about">About</a></li>
+            <li class="{{ request()->routeIs('portfolio')? 'active' : ''}}"><a href="/portfolio">Portfolio</a></li>
+            <li class="{{ request()->routeIs('contact')? 'active' : ''}}"><a href="/contact">Contact</a></li>
         </ul>
     </nav>
     @yield('content')
