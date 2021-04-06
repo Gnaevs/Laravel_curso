fab575b6637334a3f189ac8b3c672433de32311f
Romeo Lisandro Ruiz Alvarado <Gnaevs@gmail.com>
2021-04-05 23:04:23 -0500

    Cambios menores en la ortografia

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -4,7 +4,7 @@
 @section('content')
     <div class="container">
         <div class="d-flex justify-content-between align-items-center mb-3">
-        <h1 class="display-4 mb-0">Portfolio</h1>
+        <h1 class="display-4 mb-0"> {{ __('Portfolio') }} </h1>
         
 
         @auth
